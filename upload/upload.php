d7b534ecd3420668438c7e75e56c4a9d58ff3372
sweetykr7 <sweetykr@naver.com>
2023-05-19 15:46:58 +0900

    upload update

--- upload/upload.php
+++ upload/upload.php
@@ -1,9 +1,36 @@
-<!-- <?php
+#!/usr/bin/php-cgi
+<?php
+
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// 	$uploads_dir = './uploads/';
+// 	$filename = basename($_FILES['file']['name']);
+// 	$upload_path = $uploads_dir . $filename;
+// 	if (move_uploaded_file($_FILES['file']['tmp_name'], $upload_path)) {
+// 		echo 'success';
+// 		http_response_code(200);
+// 	} else {
+// 		echo 'fail';
+// 		http_response_code(500);
+// 	}
+// }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$uploads_dir = './uploads/';
 	$filename = basename($_FILES['file']['name']);
 	$upload_path = $uploads_dir . $filename;
+
+	// 파일명 중복 방지를 위한 숫자 추가
+	$counter = 1;
+	while (file_exists($upload_path)) {
+		$filename_parts = pathinfo($filename);
+		if ($filename_parts['extension'])
+			$new_filename = $filename_parts['filename'] . '-' . $counter . '.' . $filename_parts['extension'];
+		else
+			$new_filename = $filename_parts['filename'] . '-' . $counter . $filename_parts['extension'];
+		$upload_path = $uploads_dir . $new_filename;
+		$counter++;
+	}
+
 	if (move_uploaded_file($_FILES['file']['tmp_name'], $upload_path)) {
 		echo 'success';
 		http_response_code(200);
@@ -13,36 +40,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 }
 
-?> -->
 
-
-
-<?php
-echo 'pass test==========================';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $targetDir = "uploads/";  // 파일을 저장할 디렉토리
-  $targetFile = $targetDir . basename($_FILES["file"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-
-  // 파일 유효성 검사
-  if ($_FILES["file"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-  }
-  if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-  }
-
-  if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-  } else {
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
-      echo "The file " . basename($_FILES["file"]["name"]) . " has been uploaded.";
-    } else {
-      echo "Sorry, there was an error uploading your file.";
-    }
-  }
-}
 ?>
